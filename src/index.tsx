dea40538c5ff6016e9c21a0a25c176f60a54fcba
Tania <tania@MacBook-Pro-de-nico.local>
2022-10-11 11:34:33 +0200

    added search country functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { store } from "./store/index";
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
